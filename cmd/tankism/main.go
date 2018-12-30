5cb44f5150554d5345119a82b00e8c174845c005
Julian Godesa <julian.godesa@googlemail.com>
2018-12-30 14:15:36 +0100

    #1 - cleanup; rename tankismClient to tankism

---
+++ cmd/tankism/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	screenWidth  = 800
+	screenHeight = 800
+)
+
+func main() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	window, err := sdl.CreateWindow("Tankism", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		screenWidth, screenHeight, sdl.WINDOW_OPENGL)
+	if err != nil {
+		log.Println("creating window: ", err)
+		return
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		panic(err)
+	}
+
+	running := true
+	for running {
+		// event handling
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				log.Println("Quit")
+				running = false
+				break
+			}
+		}
+
+		// drawing
+		renderer.SetDrawColor(255, 244, 233, 255)
+		renderer.Clear()
+		renderer.Present()
+	}
+	sdl.Quit()
+}
