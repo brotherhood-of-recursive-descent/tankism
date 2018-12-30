168999b0faef19b12a676db012f62c8853a2c852
Julian Godesa <julian.godesa@googlemail.com>
2018-12-30 13:35:55 +0100

    extends binary to extract all existing textures into an output folder

--- cmd/sprite2png/main.go
+++ cmd/sprite2png/main.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"image"
 	"io/ioutil"
-	"log"
 	"os"
+	"path"
 	"path/filepath"
 
 	"image/png"
@@ -30,16 +30,18 @@ func main() {
 
 	// parse flags
 	var filename string
+	var outputDir string
 	flag.StringVar(&filename, "filename", "", "path to xml")
+	flag.StringVar(&outputDir, "outputDir", "output", "path to output directory")
 	flag.Parse()
 
 	if len(filename) == 0 {
-		fmt.Print("missing filename, exit.\n")
+		printUsage()
 		os.Exit(1)
 	}
 
 	// load data into struct
-	fmt.Printf("loading file '%s'...\n", filename)
+	fmt.Println("loading textureAtlas '%s'", filename)
 	xmlFile, err := os.Open(filename)
 	defer xmlFile.Close()
 
@@ -51,39 +53,84 @@ func main() {
 	b, _ := ioutil.ReadAll(xmlFile)
 	var textureAtlas TextureAtlas
 	xml.Unmarshal(b, &textureAtlas)
-	fmt.Printf("found %d textures\n", len(textureAtlas.SubTextures))
+	fmt.Println("found %d textures", len(textureAtlas.SubTextures))
 
 	// load spritesheet and extract sprites
 	basepath, _ := filepath.Abs(filepath.Dir(filename))
 	spriteFilepath := filepath.Join(basepath, textureAtlas.ImagePath)
 	spritesheet, err := os.Open(spriteFilepath)
-	fmt.Printf("loading spritesheet %s\n", spriteFilepath)
 	defer spritesheet.Close()
 
+	spritesheetImage, _ := png.Decode(spritesheet)
+
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("failed loading image referece: ", err)
+		os.Exit(1)
+	}
+	fmt.Printf("loading image reference %s\n", spriteFilepath)
+
+	// create output directory
+	err = createDirectory(outputDir)
+	if err != nil {
+		fmt.Println("failed creating outputDirectory: ", err)
 		os.Exit(1)
 	}
 
-	spritesheetImage, _ := png.Decode(spritesheet)
-	subImage := spritesheetImage.(interface {
-		SubImage(r image.Rectangle) image.Image
-	}).SubImage(image.Rect(textureAtlas.SubTextures[0].X,
-		textureAtlas.SubTextures[0].Y,
-		textureAtlas.SubTextures[0].X+textureAtlas.SubTextures[0].Width,
-		textureAtlas.SubTextures[0].Y+textureAtlas.SubTextures[0].Height))
+	// extract individual textures
+	for _, entry := range textureAtlas.SubTextures {
+		subImage, err := getSubImage(spritesheetImage, entry)
+		if err != nil {
+			fmt.Println("skipping image due to: ", err)
+			continue
+		}
 
-	f, err := os.Create("image.png")
+		if err := writePngImage(subImage, path.Join(outputDir, entry.Name)); err != nil {
+			fmt.Println("skipping image due to: ", err)
+			continue
+		}
+		fmt.Println("wrote image: ", entry.Name)
+	}
+}
+
+// getSubImage extracts a subtexture from a given spritesheet
+func getSubImage(spritesheet image.Image, texture SubTexture) (image.Image, error) {
+	subImage := spritesheet.(interface {
+		SubImage(r image.Rectangle) image.Image
+	}).SubImage(image.Rect(texture.X,
+		texture.Y,
+		texture.X+texture.Width,
+		texture.Y+texture.Height))
+
+	return subImage, nil
+}
+
+// writePngImage writes the given image to a png file
+func writePngImage(subImage image.Image, name string) error {
+	f, err := os.Create(name)
+	defer f.Close()
+
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	if err := png.Encode(f, subImage); err != nil {
-		f.Close()
-		log.Fatal(err)
+		return err
 	}
 
-	if err := f.Close(); err != nil {
-		log.Fatal(err)
+	return nil
+}
+
+// createDirectory creates the directory structure if not present
+func createDirectory(dir string) error {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err = os.MkdirAll(dir, 0755); err != nil {
+			return err
+		}
 	}
+	return nil
+}
+
+func printUsage() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+	flag.PrintDefaults()
 }
