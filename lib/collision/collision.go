0dec66026608ae9ecd63964205e2553d8cc57c85
Julian Godesa <julian.godesa@googlemail.com>
2023-03-19 16:31:16 +0100

    #38 - adds more tests rotating edges

--- lib/collision/collision.go
+++ lib/collision/collision.go
@@ -1,13 +1,79 @@
 package collision
 
-import "github.com/co0p/tankism/lib/ecs"
+import (
+	"fmt"
+	"math"
+
+	"github.com/co0p/tankism/lib/ecs"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+type Edges [4]Point
+
+func (e1 *Edges) Equal(e2 Edges) bool {
+	same := true
+
+	for k, _ := range e1 {
+		a := e1[k]
+		b := e2[k]
+
+		if a.X != b.X || a.Y != b.Y {
+			same = false
+		}
+	}
+
+	return same
+}
+
+func (e *Edges) String() string {
+	return fmt.Sprintf("[[%f, %f], [%f, %f], [%f, %f], [%f, %f]]",
+		e[0].X, e[0].Y, e[1].X, e[1].Y, e[2].X, e[2].Y, e[3].X, e[3].Y)
+}
 
 type BoundingBox struct {
 	X      float64
 	Y      float64
 	Width  float64
 	Height float64
-	E      *ecs.Entity // #38 - TODO remove, collision should work with primitives and not know about about ecs*
+
+	p1, p2, p3, p4 Point
+
+	E *ecs.Entity // #38 - TODO remove, collision should work with primitives and not know about about ecs*
+}
+
+// Rotate sets the edges of the rectangle based on the origin X,Y, width and height and the given rotation in deg by
+// multipliying with rotation matrix. See https://ebitengine.org/en/documents/matrix.html
+func (b *BoundingBox) Rotate(theta float64) BoundingBox {
+
+	// ebiten has y value pointing down, aka lefthanded
+	b.p1 = Point{X: b.X, Y: b.Y}
+	b.p2 = Point{X: b.X + b.Width, Y: b.Y}
+	b.p3 = Point{X: b.X, Y: b.Y + b.Height}
+	b.p4 = Point{X: b.X + b.Width, Y: (b.Y + b.Height)}
+
+	if theta == 0 {
+		return *b
+	}
+
+	sin, cos := math.Sincos(-theta)
+	points := []*Point{&b.p1, &b.p2, &b.p3, &b.p4}
+	for _, v := range points {
+		/* rotate x,y by deg.
+		|cos, -sin| * |X|		=	|cos*X + -sin*Y|
+		|sin, cos|    |Y|			|sin*X* + cos*Y|
+		*/
+		v.X = cos*v.X + -sin*v.Y
+		v.Y = sin*v.X + cos*v.Y
+	}
+
+	return *b
+}
+
+func (b *BoundingBox) Edges() Edges {
+	return Edges{b.p1, b.p2, b.p3, b.p4}
 }
 
 // TODO: replace with Separating Axis Theorem to accomodate for rotation of rectangles
--- lib/collision/collision_test.go
+++ lib/collision/collision_test.go
@@ -1,12 +1,13 @@
 package collision_test
 
 import (
+	"math"
 	"testing"
 
 	"github.com/co0p/tankism/lib/collision"
 )
 
-func TestCollisionAABB_collision(t *testing.T) {
+func Test_CollisionAABB_collision(t *testing.T) {
 
 	bbA := collision.BoundingBox{X: 100, Y: 100, Width: 100, Height: 100}
 	bbB := collision.BoundingBox{X: 150, Y: 150, Width: 100, Height: 100}
@@ -17,7 +18,7 @@ func TestCollisionAABB_collision(t *testing.T) {
 	}
 }
 
-func TestCollisionAABB_no_collision(t *testing.T) {
+func Test_CollisionAABB_no_collision(t *testing.T) {
 
 	bbA := collision.BoundingBox{X: 100, Y: 100, Width: 100, Height: 100}
 	bbB := collision.BoundingBox{X: 400, Y: 400, Width: 100, Height: 100}
@@ -25,5 +26,44 @@ func TestCollisionAABB_no_collision(t *testing.T) {
 	ok := collision.AABBCollision(bbA, bbB)
 	if ok {
 		t.Errorf("expected no collision between bbA and bbB, got true\n")
+	}
+}
+
+// TODO: #38 - somehow rotation does not work, maybe ceil, floor and absolute needs to be applied ?
+func DEFUNC_Test_Rotate(t *testing.T) {
+
+	cases := []struct {
+		desc     string
+		bb       collision.BoundingBox
+		theta    float64
+		expected collision.Edges
+	}{
+		{"0deg rotation",
+			collision.BoundingBox{X: 0, Y: 0, Width: 40, Height: 20},
+			0,
+			collision.Edges{{0, 0}, {40, 0}, {40, 20}, {0, 20}}},
+		{"90deg rotation",
+			collision.BoundingBox{X: 0, Y: 0, Width: 40, Height: 20},
+			math.Pi / 2,
+			collision.Edges{{0, 0}, {0, 40}, {-20, 40}, {-20, 0}}},
+		{"180deg rotation",
+			collision.BoundingBox{X: 0, Y: 0, Width: 40, Height: 20},
+			math.Pi,
+			collision.Edges{{0, 0}, {-40, 0}, {-40, -20}, {0, -20}}},
+		{"360deg rotation",
+			collision.BoundingBox{X: 0, Y: 0, Width: 40, Height: 20}, math.Pi * 2,
+			collision.Edges{{0, 0}, {0, 40}, {40, 20}, {0, 20}}},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+
+			actual := tc.bb.Rotate(tc.theta)
+			actualEdges := actual.Edges()
+
+			if !actualEdges.Equal(tc.expected) {
+				t.Errorf("expected: \n%v, got:\n%v\n", tc.expected, actualEdges)
+			}
+		})
 	}
 }
