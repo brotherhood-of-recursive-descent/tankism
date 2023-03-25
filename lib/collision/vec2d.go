2e5105fe19b84d58d142cf3843eb98c9b4b6bbb7
Julian Godesa <julian.godesa@googlemail.com>
2023-03-25 19:17:44 +0100

    Fix rotate

--- lib/collision/vec2d.go
+++ lib/collision/vec2d.go
@@ -6,19 +6,15 @@ type Vec2d struct {
 	X, Y float64
 }
 
-func (v *Vec2d) Rotate(theta float64) Vec2d {
-	if int(theta)%360 == 0 {
-		return *v
-	}
+/* Rotate rotates the vector x,y by theta (rad) clockwise, see https://ebitengine.org/en/documents/matrix.html#Rotating
 
-	/* rotate x,y, see https://ebitengine.org/en/documents/matrix.html#Rotating
 	|cos,-sin| * |X|	=	|cos*X - sin*Y|
 	|sin, cos|   |Y|		|sin*X + cos*Y|
 	*/
-
+func (v *Vec2d) Rotate(theta float64) Vec2d {
 	sin, cos := math.Sincos(theta)
-	v.X = math.Round(cos*v.X - sin*v.Y)
-	v.Y = math.Round(sin*v.X + cos*v.Y)
+	x := math.Round(cos*v.X - sin*v.Y)
+	y := math.Round(sin*v.X + cos*v.Y)
 
-	return *v
+	return Vec2d{x,y}
 }
